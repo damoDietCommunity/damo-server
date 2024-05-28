73adf4aa974ba473a8ba7bcf9f660a552a682f5a
wodydl0 <poty9798@naver.com>
2024-05-28 21:04:39 +0900

    배포 설정

--- src/main/java/com/example/damoserver/chat/handler/WebSocketEventListener.java
+++ src/main/java/com/example/damoserver/chat/handler/WebSocketEventListener.java
@@ -3,6 +3,7 @@ package com.example.damoserver.chat.handler;
 import com.example.damoserver.chat.entity.Chat;
 import com.example.damoserver.chat.entity.ChatType;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -16,15 +17,16 @@ import java.util.logging.Logger;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
-    private final Logger logger = (Logger) LoggerFactory.getLogger(WebSocketEventListener.class);
+
 
 
     private SimpMessagingTemplate messagingTemplate;
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-        logger.info("New connection established");
+        log.info("New connection established");
     }
 
     @EventListener
@@ -33,7 +35,7 @@ public class WebSocketEventListener {
 
         String username = (String) accessor.getSessionAttributes().get("username");
         if (username != null) {
-            logger.info("Disconnected from " + username);
+            log.info("Disconnected from " + username);
 
             Chat chat = Chat.builder()
                     .chatType(ChatType.LEAVE)
--- src/main/java/com/example/damoserver/config/WebSocketConfig.java
+++ src/main/java/com/example/damoserver/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.example.damoserver.config;
 
-import lombok.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
